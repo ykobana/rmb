c8fa6d8da2be34be2daefbbeb5be6477c403aa71
小花祐輔 <kobanayusuke@kobanayuusukenoMacBook-Pro.local>
2018-12-16 15:26:32 +0900

    DB追加

--- rmb/app/models.py
+++ rmb/app/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+
+class Character(models.Model):
+    rate = models.IntegerField(default=1000)
+    level = models.IntegerField(default=1)
+    experience_point = models.IntegerField(default=0)
+    hit_point = models.IntegerField(default=100)
+    magic_point = models.IntegerField(default=150)
+    power = models.IntegerField(default=150)
+    defence = models.IntegerField(default=150)
+    speed = models.IntegerField(default=150)
+    luck = models.IntegerField(default=150)
+    skill1 = models.IntegerField(default=1)
+    skill2 = models.IntegerField(default=0)
+    skill3 = models.IntegerField(default=0)
+    skill4 = models.IntegerField(default=0)
+
+
+class UserAndCharacterLink(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    character_id = models.ForeignKey(Character, on_delete=models.CASCADE)
+
+
+class BattleResult(models.Model):
+    characterA = models.ForeignKey(UserAndCharacterLink, on_delete=models.CASCADE, related_name='A')
+    characterB = models.ForeignKey(UserAndCharacterLink, on_delete=models.CASCADE, related_name='B')
+    result = models.IntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(2)])  # 勝ち/負け
+
+
+class Skill(models.Model):
+    power = models.IntegerField(default=10)
+    magic_point_consumption = models.IntegerField(default=0)
+
