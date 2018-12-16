cf8f123f6af8732caf1be44d3325a3b88e02c61a
Yusuke KOBANA <ykoba16@gmail.com>
2018-12-16 20:24:34 +0900

    ログイン画面のモック作成

--- rmb/app/views.py
+++ rmb/app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views import generic
 
-# Create your views here.
+
+@method_decorator(login_required, name='dispatch')
+class IndexView(generic.ListView):
+    template_name = 'app/login.html'
 
 
 def index(request):
